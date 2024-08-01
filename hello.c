4095cec5d09a20051cc89a3653242e077beb2c1e
Drew Anello <drew.anello@gmail.com>
2024-07-31 22:44:53 -0400

    adds

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 2;
+    if (x > 4)
+    {
+        printf("x is greater than 4\n");
+    }
+    else
+    {
+        printf("x is not greater than 4\n");
+    }
+}
