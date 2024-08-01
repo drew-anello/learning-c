10e8f5a411d8936f067e606f2961f8bcde1fad23
Drew Anello <drew.anello@gmail.com>
2024-07-31 22:51:54 -0400

    adds ability for user input name

--- hello.c
+++ hello.c
@@ -2,13 +2,19 @@
 
 int main(void)
 {
-    int x = 2;
-    if (x > 4)
-    {
-        printf("x is greater than 4\n");
-    }
-    else
-    {
-        printf("x is not greater than 4\n");
-    }
+    // int x = 2;
+    // if (x > 4)
+    // {
+    //     printf("x is greater than 4\n");
+    // }
+    // else
+    // {
+    //     printf("x is not greater than 4\n");
+    // }
+
+    char name[] = "";
+
+    printf("what's your name? \n");
+    scanf("%s", name);
+    printf("Hello, %s\n", name);
 }
